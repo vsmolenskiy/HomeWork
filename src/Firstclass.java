f600f5289e179bee6bbe53900a009ea59db767db
vsmolenskiy <remedilessoptimist@gmail.com>
2017-01-19 15:58:03 +0200

    Home work update

---
+++ src/Firstclass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by V.Smolenskyi on 19.01.2017.
+ */
+import java.util.Scanner;
+public class Firstclass {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+    }
+}
